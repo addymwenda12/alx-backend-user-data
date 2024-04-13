f4f044b4c7ffd5342b8c9eeba00a26f70761fc11
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-04-13 13:13:26 +0300

    Created get_logger function and define PII_FIELDS constant

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,6 +7,8 @@ import re
 import logging
 from typing import List, Tuple
 
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
@@ -63,3 +65,27 @@ class RedactingFormatter(logging.Formatter):
         original_message = logging.Formatter.format(self, record)
         return filter_datum(self.fields,
                             self.REDACTION, original_message, self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """
+    Returns a logging.Logger object.
+
+    The logger is named "user_data", logs up to logging.INFO level,
+    does not propagate messages to other loggers,
+    and has a StreamHandler with RedactingFormatter as formatter.
+
+    Returns:
+        logging.Logger: The configured logger.
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    formatter = RedactingFormatter(PII_FIELDS)
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+
+    return logger
