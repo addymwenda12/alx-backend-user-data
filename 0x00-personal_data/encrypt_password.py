7f71160f38cec56d3966ce2dfe7cbabae3749542
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-04-13 13:37:33 +0300

    Implemented password hashing and validation functions

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -20,3 +20,18 @@ def hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode(), salt)
 
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validates that the provided password matches the
+    hashed password.
+
+    Args:
+        hashed_password (bytes): The hashed password.
+        password (str): The password to validate.
+
+    Returns:
+        bool: True if the password is valid, False otherwise.
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
