83f8620a12e83a747f79a87b85cb93860d9a11e9
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-04-26 15:32:52 +0300

    Add POST /users route to Flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,9 +2,11 @@
 """A simple Flask app with user authentication features.
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
@@ -15,6 +17,24 @@ def welcome() -> str:
         - JSON payload containing a welcome message.
     """
     return jsonify({"message": "Bienveneu"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """
+    POST /users
+    Return:
+        - JSON payload containing a success message if the user is registered successfully.
+        - JSON payload containing an error message if the user is already registered.
+    """
+    email = request.form['email']
+    password = request.form['password']
+
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
