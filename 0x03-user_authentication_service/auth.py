8a14558b09436901223f5dd40262fd58546b7c42
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-04-26 14:40:45 +0300

    Implemented _hash_password function in auth module

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+This module contains the Auth class which provides authentication
+for the user
+"""
+
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """
+    Hash a password with bcrypt.
+
+    Args:
+        password (str): The password to hash.
+
+    Returns:
+        bytes: The hashed password.
+    """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode(), salt)
+
+    return hashed_password
