0d89824a9124609e4586d1f385b1b047b1f2ccc2
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-04-26 17:37:24 +0300

    Implemented profile route and user email retrieval

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -80,5 +80,23 @@ def logout() -> str:
     return redirect('/')
 
 
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile() -> str:
+    """
+    GET /profile
+    Return:
+        - JSON payload containing the user's email
+        if the user is found.
+        - 403 HTTP status if the session ID is invalid or the user does not exist.
+    """
+    session_id = request.cookies.get('session_id')
+
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+
+    return jsonify({"email": user.email}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
