b8239a1cb468335bd33bf25de599aae52e737e3a
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-04-26 14:01:06 +0300

    Implemented add_user method in DB class

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""
+This module contains the DB class which provides access to the database.
+
+The DB class is responsible for creating the database engine, managing the
+database session, and providing methods to interact with the database.
+Currently, it supports adding a new user to the database.
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import User, Base
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """
+        Add a new user to the database.
+
+        Args:
+            email (str): The email of the user.
+            hashed_password (str): The hashed password of the user.
+
+        Returns:
+            User: The created User object.
+        """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
